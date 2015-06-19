b4420dedf986d7a348e9f5c086230ff882c23e5e
AlexCrespo <Alumno@DAM118>
2015-06-19 19:34:11 +0200

    Alex

---
+++ src/Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+
+	/**
+	 * @author Alex Crespo
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ALEX CRESPO
+
+	}
+
+}
